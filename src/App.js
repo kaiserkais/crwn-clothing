2afbcc9467c08e157d227e9f797680d8273c736f
kais <kais.seven@gmail.com>
2019-09-05 14:30:17 +0100

    updating our app with shop page, commection preview and item

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import React from 'react';
 import {Switch ,Route} from 'react-router-dom'
-
+import ShopPage from './pages/shop/shop.component'
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
-const HatsPage = () => (
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-)
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route exact path='/hats' component={HatsPage} />
+        <Route exact path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
